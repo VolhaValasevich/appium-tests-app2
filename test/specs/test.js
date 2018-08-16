d8a0c31fa29510b372a6c494d8603a634e2684a5
Volha Valasevich <volha_valasevich@epam.com>
2018-08-16 14:14:24 +0300

    git add tests + remove appium-doctor

--- test/specs/test.js
+++ test/specs/test.js
@@ -1,7 +1,33 @@
-describe('App2', () => {
+describe('App2 ', () => {
 
-    it('open an app', () => {
-        console.log('It works!');
+    beforeAll(() => {
+        browser.timeouts('implicit', 10000);
+        browser.click('#sign_in');
+        browser.element('#username').setValue('testautomationuser');
+        browser.element('#password').setValue('testautomation');
+        browser.back();
+        browser.click('#sign_in');
+    });
+
+    it('should have a greeting', () => {
+        expect(browser.getText('#engagement_title')).toEqual('Добро пожаловать в ProtonMail!');
+    });
+
+    it('should have a close button', () => {
+        expect(browser.getText('#button_close')).toEqual('ЗАКРЫТЬ ТУР');
+    });
+
+    describe("Inbox", () => {
+
+        beforeAll(() => {
+            browser.click('#button_close');
+        });
+
+        it('should have title', () => {
+            const selector = 'new UiSelector().text("ВХОДЯЩИЕ")';
+            console.log(browser.element('android=' + selector));
+
+        });
     });
 
 });
